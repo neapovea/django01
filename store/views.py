e1a9c93cbff41e23821a38ddc59bac52a5a6e33a
neapovea <neapovea@gmail.com>
2017-04-13 18:16:29 +0200

    Refactored base template, include background image in static files directory

--- store/views.py
+++ store/views.py
@@ -8,12 +8,13 @@ def index(request):
 def store(request):
     count = Book.objects.all().count()
     context = {'count': count,
+               'page':'welcome to mystery books!',
                }
 
     request.session['location'] = "NO SE SABE"
     if request.user.is_authenticated():
         request.session['location'] = "la TIERRA"
 
-    return render(request, 'store.html', context)
+    return render(request, 'base.html', context)
 
     #version_anterio return render(request, 'store.html')
